a64475a037e3c355849c7d2134ea83eeaa148570
Marcelo de Oliveira Carneiro <marcelo.oliveira.carneiro@gmail>
2024-12-03 13:32:43 -0300

    Refactoring

--- app.js
+++ app.js
@@ -3,17 +3,21 @@ let numeroSecreto = 0;
 let numeroTentativas = 0;
 let numeroMaximo = 4;
 
+document.getElementById('reiniciar').click();
 novoJogo();
 
 function novoJogo() {
     numeroSecreto = gerarNumeroAleatorio();
     numeroTentativas = 0;
-    
+
     setSelectorInnerHTML('h1', 'Jogo do número secreto'); 
     setSelectorInnerHTML('p', 'Escolha um número entre 1 e 10');
+    
     document.getElementById('reiniciar').setAttribute('disabled',true);
     limparCampo();
 }
+
+
 
 function verificarChute() { 
     numeroTentativas ++;
@@ -26,7 +30,7 @@ function verificarChute() {
         limparCampo();
         setSelectorInnerHTML('p', `:( O número é ${((chute < numeroSecreto) ? 'maior' : 'menor')} que ${chute}`);
     }
-}
+} 
 
 function gerarNumeroAleatorio() {
     let numeroAleatorio = parseInt(Math.random() * numeroMaximo + 1);
@@ -37,7 +41,7 @@ function gerarNumeroAleatorio() {
         return gerarNumeroAleatorio();
     } else {
         numerosSorteados.push(numeroAleatorio);
-        console.log(numerosSorteados);
+        //console.log(numerosSorteados);
         return numeroAleatorio;
     }
     return parseInt(Math.random() * 10 + 1);
@@ -47,29 +51,29 @@ function limparCampo() {
     document.querySelector('input').value = '';
 }
 
-function setSelectorInnerHTML(tag, texto) {
+function setSelectorInnerHTML(tag, texto) { 
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
-    speakText( texto, 2 ); 
+    speakText( texto, 1 );   
 }
 
-function speakText( text, library ) {
+function speakText( text, library ) { 
 /*    
     if (library == 1) {
-        responsiveVoice.speak( text, 'Brazilian Portuguese Female', {rate:1.2});
+        //responsiveVoice.speak( text, 'Brazilian Portuguese Female', {rate:1.2});
     }
-    else {
+    else {  
 */        
         if ('speechSynthesis' in window) {
             let utterance = new SpeechSynthesisUtterance(text);
             utterance.lang = 'pt-BR'; 
-            SpeechSynthesisVoice
+            utterance.rate = 1.2;
             window.speechSynthesis.speak(utterance); 
         } else {
             console.log("Web Speech API não suportada neste navegador.");
         }
-//    }
-}
+  //  }
+} 
 
 
 
